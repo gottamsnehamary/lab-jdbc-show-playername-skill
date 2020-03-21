991a6ec2251809f443af83e11d4d4aa3c1927451
gottamsnehamary <61041030+gottamsnehamary@users.noreply.github.com>
2020-03-21 23:48:54 +0530

    lab-jdbc-show-playername-skill

--- src/dao/PlayerDAO.java
+++ src/dao/PlayerDAO.java
@@ -1,1 +1,43 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Player;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class PlayerDAO
+{
+	 static List<Player> list=new ArrayList<Player>();
+	 
+	public List<Player> getAllPlayers() throws Exception 
+	{
+		Connection con=ConnectionManager.getConnection();
+		 Statement stmt=  con.createStatement();
+			
+			ResultSet rs =stmt.executeQuery("select * from player FULL OUTER JOIN skill  ON  player.skill_id=skill.id");
+		  
+			while(rs.next())
+		    {
+				long skillId=rs.getLong(4);
+				long playerId=rs.getLong(1);
+				String playerName=rs.getString(2);
+				String playerCountry=rs.getString(3);
+				String skillName=rs.getString(6);
+				
+				Skill skill=new Skill(skillId,skillName);
+				
+				Player player =new Player(playerId,playerName,playerCountry,skill);
+				
+		 		list.add(player);
+		
+		    }
+	
+		return list;
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,44 @@
 
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	 public Skill getSkillBylD(Long id) throws Exception 
+	{
+		 Connection con=ConnectionManager.getConnection();
+		 
+		 Statement stmt=  con.createStatement();
+			
+			ResultSet rs =stmt.executeQuery("select * from skill ");
+			Skill skill=new Skill();
+			while(rs.next())
+			{
+				long id1=rs.getLong(1);
+				if(id1==id)
+				{
+				
+				String name=rs.getString(2);
+				
+				
+				skill.setSkilld(id1);
+				skill.setSkillName(name);
+				
+				return skill;
+				}
+				
+				
+			}
+		
+		return null;
+	 
+	}
+	 
+}
+
